8e93aadbdd6e3cd1eaa38c37dc1ebab4f111e29b
JonasNev <J.nevinskas@gmail.com>
2021-05-13 12:54:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,106 @@
 document.addEventListener('click', function(event) {
-    var target = event.target
-    
+    var target = event.target;
     markX(target);
 });
 
 function markX(element){
     if (element.classList.contains("cell") && element.innerText == ""){
         element.innerText = "X";
-        opponentMove();
+
+        let finished = isGameFinished();
+
+        if (finished == false) {
+            opponentMove();
+            isGameFinished();
+        }
     }
     else {
-        // Nieko nedarys
+        // Do nothing
     }
 }
 
+
 function opponentMove(){
-    //Oponento judesys
 
-    //Gauti visus galimus veiksmus
-    let elements = Array.from(document.querySelectorAll(".cell"));
-    elements = elements.filter(function(el){
-        return el.innerText == ""
-    });
+    // Get all elements with class name
+    var cells = Array.from(document.getElementsByClassName("cell"));
+    // Filter the elements by just selecting empty ones.
+      
+    cells = cells.filter(cell => cell.innerText == "");
+
+    // Get random number
+    let randomNumber = getRandomNumber(0, cells.length);
+
+    // Select random element 
+
+    let randomCell = cells[randomNumber];
     
-    let randomNo = randomInteger(0, elements.length);
+    //and modify its innertext to O
 
-    let selectedElement = elements[randomNo];
-
-    selectedElement.innerText = "O";
-
-    //Issirinkti viena is galimu variantu
-    //PAzymeti elementa, kaip 0
+    randomCell.innerText = '0'
 }
+  
 
-function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-function getRandomMove(){
-    // Analizuoja ir nuskaito koki judesi padaryt
-}
-
-function whoWon(){
-    //Isanalizuoja lenta ir nusprendzia kas laimejo, jeigu laimejo. 
+function getRandomNumber(min, max){
+    const r = Math.random()*(max-min) + min
+    return Math.floor(r)
 }
 
 function isGameFinished(){
+    return isWinnerDetermined() || noMovesLeft();
+}
 
+function noMovesLeft(){
+    var cells = Array.from(document.getElementsByClassName("cell"));
+
+    cells = cells.filter(cell => cell.innerText == "");
+
+    if (cells.length == 0){
+        //modify .game--status to be draw
+        var gameStatusElement = document.querySelector(".game--status");
+
+        gameStatusElement.innerText = "DRAW";
+        return true; //the game is finished
+    }
+
+    return false; // the game continues
+}
+
+
+function isWinnerDetermined(){
+    let winningCellCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+        //Go  through there winning combinations
+        winningCellCombinations.forEach(combination =>
+            {
+                elementValue1 = getDataCellTextValueByIndex(combination[0]);
+                elementValue2 = getDataCellTextValueByIndex(combination[1]);
+                elementValue3 = getDataCellTextValueByIndex(combination[2]);
+                
+                if (elementValue1 == elementValue2 && elementValue2 == elementValue3 && elementValue1 != ""){
+                    var gameStatusElement = document.querySelector(".game--status");
+
+                    gameStatusElement.innerText = `${elementValue1} has won the game`
+                   
+                }
+                return true;
+            });
+
+            return false;
+}
+
+function getDataCellTextValueByIndex(index){
+    return document.querySelector(`[data-cell-index="${index}"]`).innerText;
 }
 
 function resetGame(){
 
 }
-
